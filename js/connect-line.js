8824748a60232a03488f95e404ffd39f40976bdd
篠原 俊弥 <toshiya.shinohara@doubleloop.co.jp>
2018-06-17 09:46:55 +0900

    要素がいっぱい出来ちゃうのを修正

--- js/connect-line.js
+++ js/connect-line.js
@@ -1,13 +1,13 @@
 function connectLine(className, i = 0) {
     var classNameId = className.replace('.', '');
     var array = [];
+
+    //要素の位置を取得
     $(className).each(function (index) {
         var datatop = $(this).offset().top;
         var dataleft = $(this).offset().left;
         array[i] = { "datatop": datatop, "dataleft": dataleft };
         $(this).attr("data-top", datatop).attr("data-left", dataleft);
-        //$(this).attr("id", classNameId + (i + 1));
-        $(this).wrapInner('<span class="connect-line-point"></span>');
         i++;
     });
 
@@ -23,13 +23,20 @@ function connectLine(className, i = 0) {
     var lineZ = lineWidth / Math.sin(rad);
 
     //直角で狂うのを回避
-    if (degree == 0 || degree == 180){
+    if (degree == 0 || degree == 180) {
         lineZ = lineHeight;
-    }else if (degree == 90 || degree == 270){
+    } else if (degree == 90 || degree == 270) {
         lineZ = lineWidth;
     };
 
     $(className).css("position", "relative");
-    $(className + ":first").append('<span class="connect-line-line connect-line-' + classNameId + '"></span>');
+
+    //最初の一回だけ実行
+    if (!$('.connect-line-' + classNameId).length) {
+        $(className + ":first").append('<span class="connect-line-line connect-line-' + classNameId + '"></span>');
+        $(className).wrapInner('<span class="connect-line-point"></span>');
+        $(className + ":first connect-line-point").addClass('connect-line-point-first');
+    }
+
     $(".connect-line-" + classNameId).css("transform", "rotate(" + -degree + "deg)").css("height", lineZ + "px").css("background", "red");
 }
